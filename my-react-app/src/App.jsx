f2214583d5d207d72512a763b7fac302cf0d0b2c
William Matos <wiusk8@gmail.com>
2024-09-17 20:13:47 -0300

    genshin wiki v2

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,29 @@
+import React, { useEffect, useState } from 'react';
 import Layout from "./components/Layout/layout";
 import './app.css'
+import { fetchCharacters } from './api';
 
 function App() {
 
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    const getCharacters = async () => {
+      const data = await fetchCharacters();
+      setCharacters(data);
+    };
+
+    getCharacters();
+  }, []);
+
   return (
     <Layout>
-      <div className="content-homepage">
-        
+      <div className="centerdv">
+        <div className="container-images">
+          {characters.map((character) => (       
+              <img src={character.bgImage} alt="image" />
+          ))}  
+      </div>
       </div>
     </Layout>
   )
