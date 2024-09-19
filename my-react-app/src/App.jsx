05a065d749eb445628ae8a28895d48d8c9e98e35
William Matos <wiusk8@gmail.com>
2024-09-19 16:28:40 -0300

    resolve app css

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Layout from "./components/Layout/layout";
-import './app.css'
+import './App.css'
 import { fetchCharacters } from './api';
 
 function App() {
